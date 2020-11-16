31313a5c0337f2cb5c0ad8f84157a2efbc79db5e
aderibigbeganiu <aderibigbeganiu@gmail.com>
2020-11-16 11:06:21 +0100

    initial commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -48,7 +48,7 @@ const Navbar = () => {
             <Link
               className={navStyle.navItem}
               activeClassName={navStyle.activeNavItem}
-              to="/abouts"
+              to="/about"
             >
               ABOUT US
             </Link>
