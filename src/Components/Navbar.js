f503662c4a9803e9bd35255a8b37b39fca7fdb21
aderibigbeganiu <aderibigbeganiu@gmail.com>
2020-11-16 13:14:41 +0100

    initial commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,85 +4,151 @@ import navStyle from "./navStyle.module.scss"
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg m-0 p-0">
-      <Link className="navbar-brand" to="/">
-        <img
-          src="https://kappohotels.com/wp-content/uploads/2020/05/cropped-hotel-LOGO-scaled-1-192x192.jpg"
-          alt="home"
-          height="80"
-          width="100"
-        />
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item active">
-            <Link
-              className={navStyle.navItem}
-              activeClassName={navStyle.activeNavItem}
-              to="/"
-            >
-              HOME
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className={navStyle.navItem}
-              activeClassName={navStyle.activeNavItem}
-              to="/gallery"
-            >
-              GALLERY
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className={navStyle.navItem}
-              activeClassName={navStyle.activeNavItem}
-              to="/about"
-            >
-              ABOUT US
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className={navStyle.navItem}
-              activeClassName={navStyle.activeNavItem}
-              to="services"
-            >
-              SERVICES & AMENITIES
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className={navStyle.navItem}
-              activeClassName={navStyle.activeNavItem}
-              to="/contact"
-            >
-              CONTACT
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className={navStyle.navItem}
-              activeClassName={navStyle.activeNavItem}
-              to="/Offer"
-            >
-              OFFERS
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar navbar-expand-md m-0 p-0 d-none d-md-block">
+        <Link className="navbar-brand" to="/">
+          <img
+            src="https://kappohotels.com/wp-content/uploads/2020/05/cropped-hotel-LOGO-scaled-1-192x192.jpg"
+            alt="home"
+            height="80"
+            width="100"
+          />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <Link
+                className={navStyle.navItem}
+                activeClassName={navStyle.activeNavItem}
+                to="/"
+              >
+                HOME
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={navStyle.navItem}
+                activeClassName={navStyle.activeNavItem}
+                to="/gallery"
+              >
+                GALLERY
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={navStyle.navItem}
+                activeClassName={navStyle.activeNavItem}
+                to="/about"
+              >
+                ABOUT US
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={navStyle.navItem}
+                activeClassName={navStyle.activeNavItem}
+                to="services"
+              >
+                SERVICES & AMENITIES
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={navStyle.navItem}
+                activeClassName={navStyle.activeNavItem}
+                to="/contact"
+              >
+                CONTACT
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={navStyle.navItem}
+                activeClassName={navStyle.activeNavItem}
+                to="/Offer"
+              >
+                OFFERS
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <nav class="navbar navbar-dark d-sm-block d-md-none">
+        <Link class="navbar-brand" to="/">
+          <img
+            src="https://kappohotels.com/wp-content/uploads/2020/05/cropped-hotel-LOGO-scaled-1-192x192.jpg"
+            alt="home"
+            height="50"
+            width="60"
+          />
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarsExample01"
+          aria-controls="navbarsExample01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample01">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">
+                Disabled
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="dropdown01"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <div class="dropdown-menu" aria-labelledby="dropdown01">
+                <a class="dropdown-item" href="#">
+                  Action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,101 @@
 import React from "react"
 import Layout from "../Components/Layout"
+import { Col, Row } from "react-bootstrap"
+import indexStyle from "./indexStyle.module.scss"
 const Home = () => {
-  return <Layout name="Home"></Layout>
+  return (
+    <Layout name="Home">
+      <Row className={indexStyle.row}>
+        <Col md={6} className="p-5">
+          <img
+            src="https://thegeorgelagos.com/wp-content/uploads/2018/10/about-us-1.png"
+            alt="fancy-img"
+            className="img-fluid"
+            height="652"
+            width="602"
+          />
+        </Col>
+        <Col md={6} className="p-5">
+          <h2 className={indexStyle.staySafe}>Stay Safe!</h2>
+          <h4 className={indexStyle.welcome}>Welcome to</h4>
+          <h1 className={indexStyle.title}>KAPPO HOTEL & SUITES</h1>
+          <p className={indexStyle.p}>
+            Curabitur aliquet quam id dui posuere blandit. Nulla porttitor
+            accumsan tincidunt. Vestibulum ac diam sit amet quam vehicula
+            elementum sed sit amet dui. Pellentesque in ipsum id orci porta
+            dapibus. Curabitur non nulla sit amet nisl Curabitur aliquet quam id
+            dui posuere blandit. Pellentesque in ipsum id orci porta dapibus.
+            Sed porttitor lectus nibh. Proin eget tortor risus. Donec
+            sollicitudin molestie malesuada. Curabitur non nulla sit amet nisl
+            tempus
+          </p>
+        </Col>
+      </Row>
+      <nav>
+        <div className="nav nav-tabs" id="nav-tab" role="tablist">
+          <a
+            className="nav-link active"
+            id="nav-home-tab"
+            data-toggle="tab"
+            href="#nav-home"
+            role="tab"
+            aria-controls="nav-home"
+            aria-selected="true"
+          >
+            Home
+          </a>
+          <a
+            className="nav-link"
+            id="nav-profile-tab"
+            data-toggle="tab"
+            href="#nav-profile"
+            role="tab"
+            aria-controls="nav-profile"
+            aria-selected="false"
+          >
+            Profile
+          </a>
+          <a
+            className="nav-link"
+            id="nav-contact-tab"
+            data-toggle="tab"
+            href="#nav-contact"
+            role="tab"
+            aria-controls="nav-contact"
+            aria-selected="false"
+          >
+            Contact
+          </a>
+        </div>
+      </nav>
+      <div className="tab-content" id="nav-tabContent">
+        <div
+          className="tab-pane fade show active"
+          id="nav-home"
+          role="tabpanel"
+          aria-labelledby="nav-home-tab"
+        >
+          Home
+        </div>
+        <div
+          className="tab-pane fade"
+          id="nav-profile"
+          role="tabpanel"
+          aria-labelledby="nav-profile-tab"
+        >
+          Profile
+        </div>
+        <div
+          className="tab-pane fade"
+          id="nav-contact"
+          role="tabpanel"
+          aria-labelledby="nav-contact-tab"
+        >
+          Contact
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default Home
