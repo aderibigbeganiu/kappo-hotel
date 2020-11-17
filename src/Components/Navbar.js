31b0ce52bc2668e954a0e38f85362c630d64efab
aderibigbeganiu <aderibigbeganiu@gmail.com>
2020-11-17 14:20:47 +0100

    cover and nav fixed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,85 +5,76 @@ import navStyle from "./navStyle.module.scss"
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-md m-0 p-0 d-none d-md-block">
-        <Link className="navbar-brand" to="/">
-          <img
-            src="https://kappohotels.com/wp-content/uploads/2020/05/cropped-hotel-LOGO-scaled-1-192x192.jpg"
-            alt="home"
-            height="80"
-            width="100"
-          />
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <Link
-                className={navStyle.navItem}
-                activeClassName={navStyle.activeNavItem}
-                to="/"
-              >
-                HOME
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                className={navStyle.navItem}
-                activeClassName={navStyle.activeNavItem}
-                to="/gallery"
-              >
-                GALLERY
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                className={navStyle.navItem}
-                activeClassName={navStyle.activeNavItem}
-                to="/about"
-              >
-                ABOUT US
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                className={navStyle.navItem}
-                activeClassName={navStyle.activeNavItem}
-                to="services"
-              >
-                SERVICES & AMENITIES
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                className={navStyle.navItem}
-                activeClassName={navStyle.activeNavItem}
-                to="/contact"
-              >
-                CONTACT
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                className={navStyle.navItem}
-                activeClassName={navStyle.activeNavItem}
-                to="/Offer"
-              >
-                OFFERS
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <span className="d-none d-md-block">
+        <nav className="navbar navbar-expand-md m-0 p-0">
+          <Link className="navbar-brand" to="/">
+            <img
+              src="https://kappohotels.com/wp-content/uploads/2020/05/cropped-hotel-LOGO-scaled-1-192x192.jpg"
+              alt="home"
+              height="80"
+              width="100"
+            />
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link
+                  className={navStyle.navItem}
+                  activeClassName={navStyle.activeNavItem}
+                  to="/"
+                >
+                  HOME
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className={navStyle.navItem}
+                  activeClassName={navStyle.activeNavItem}
+                  to="/gallery"
+                >
+                  GALLERY
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className={navStyle.navItem}
+                  activeClassName={navStyle.activeNavItem}
+                  to="/about"
+                >
+                  ABOUT US
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className={navStyle.navItem}
+                  activeClassName={navStyle.activeNavItem}
+                  to="services"
+                >
+                  SERVICES & AMENITIES
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className={navStyle.navItem}
+                  activeClassName={navStyle.activeNavItem}
+                  to="/contact"
+                >
+                  CONTACT
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className={navStyle.navItem}
+                  activeClassName={navStyle.activeNavItem}
+                  to="/Offer"
+                >
+                  OFFERS
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </span>
       <nav class="navbar navbar-dark d-sm-block d-md-none">
         <Link class="navbar-brand" to="/">
           <img
