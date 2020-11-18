698c4a61188e9819497b5c647956600a0e11350e
aderibigbeganiu <aderibigbeganiu@gmail.com>
2020-11-18 19:15:42 +0100

    cpanel config

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Col, Row } from "react-bootstrap"
+import Layout from "../Components/Layout"
+
+function Contact() {
+  return (
+    <Layout>
+      <Row>
+        <Col>1</Col>
+        <Col>2</Col>
+      </Row>
+    </Layout>
+  )
+}
+
+export default Contact
